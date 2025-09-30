125693a8838590ef10c8a7f69dde0878aecf2ba6
andev18 <andev18fullstack@gmail.com>
2025-09-30 22:09:42 +0700

    fix: polish code

--- app/sidekiq/flag_fraud_if_suspended_stripe_fingerprint_worker.rb
+++ app/sidekiq/flag_fraud_if_suspended_stripe_fingerprint_worker.rb
@@ -11,11 +11,10 @@ class FlagFraudIfSuspendedStripeFingerprintWorker
     bank_account = user.active_bank_account
     return if bank_account.blank? || bank_account.stripe_fingerprint.blank?
 
-    banned_accounts_with_same_fingerprint = User.joins(:bank_accounts)
+    suspended_accounts_with_same_fingerprint = User.joins(:bank_accounts)
                                                 .where(bank_accounts: { stripe_fingerprint: bank_account.stripe_fingerprint })
                                                 .where(user_risk_state: ["suspended_for_tos_violation", "suspended_for_fraud"])
-                                                .where.not(id: user.id)
 
-    user.flag_for_fraud!(author_name: "FlagFraudIfSuspendedStripeFingerprint") if banned_accounts_with_same_fingerprint.exists?
+    user.flag_for_fraud!(author_name: "FlagFraudIfSuspendedStripeFingerprint") if suspended_accounts_with_same_fingerprint.exists?
   end
 end
