7137e65ac1c279d1cbd3d8155d53c289a2dcfc92
andev18 <andev18fullstack@gmail.com>
2025-10-28 17:11:33 +0700

    Update app/models/user.rb
    Co-authored-by: Emmanuel Cousin <EmCousin@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -170,8 +170,7 @@ class User < ApplicationRecord
                        if: :username_changed? # validate only when seller changes their username
 
   validates :name, length: { maximum: MAX_LENGTH_NAME, too_long: "Your name is too long. Please try again with a shorter one." },
-                   format: { without: INVALID_NAME_FOR_EMAIL_DELIVERY_REGEX, message: "cannot contain colons (:) as it causes email delivery problems. Please remove any colons from your name and try again." },
-                   if: :name_changed?
+                   format: { without: INVALID_NAME_FOR_EMAIL_DELIVERY_REGEX, message: "cannot contain colons (:) as it causes email delivery problems. Please remove any colons from your name and try again.", if: :name_changed? }
   validates :facebook_meta_tag, length: { maximum: MAX_LENGTH_FACEBOOK_META_TAG }
   validates :purchasing_power_parity_limit, allow_nil: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
 
