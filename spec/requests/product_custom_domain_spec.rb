bf36162e742bde305e9c36b48283a26b6032cd94
andev18 <andev18fullstack@gmail.com>
2026-03-24 19:43:40 +0700

    chore: polish tests

--- spec/requests/product_custom_domain_spec.rb
+++ spec/requests/product_custom_domain_spec.rb
@@ -38,26 +38,22 @@ describe "ProductCustomDomainScenario", type: :system, js: true do
 
   context "when there is a discount code" do
     let(:product) { create(:product, price_cents: 600) }
-    let(:offer_code) { create(:percentage_offer_code, products: [product], code: "launch") }
+    let(:offer_code) { create(:percentage_offer_code, products: [product], code: "launch", amount_percentage: 50) }
 
-    context "when the URL contains only the discount code" do
-      it "applies the discount code" do
-        visit "http://#{custom_domain.domain}:#{port}/#{offer_code.code}"
-        expect(page).to have_text("50% off will be applied at checkout (Code LAUNCH)")
-        click_on "I want this!"
-        check_out(product)
-        expect(product.sales.successful.count).to eq(1)
-      end
+    it "applies the discount code when the URL contains only the discount code" do
+      visit "http://#{custom_domain.domain}:#{port}/#{offer_code.code}"
+      expect(page).to have_text("50% off will be applied at checkout (Code LAUNCH)")
+      click_on "I want this!"
+      check_out(product)
+      expect(product.sales.successful.count).to eq(1)
     end
 
-    context "when the URL contains both the discount code and the product" do
-      it "applies the discount code" do
-        visit "http://#{custom_domain.domain}:#{port}/l/#{product.unique_permalink}/#{offer_code.code}"
-        expect(page).to have_text("50% off will be applied at checkout (Code LAUNCH)")
-        click_on "I want this!"
-        check_out(product)
-        expect(product.sales.successful.count).to eq(1)
-      end
+    it "applies the discount code when the URL contains both the discount code and the product" do
+      visit "http://#{custom_domain.domain}:#{port}/l/#{product.unique_permalink}/#{offer_code.code}"
+      expect(page).to have_text("50% off will be applied at checkout (Code LAUNCH)")
+      click_on "I want this!"
+      check_out(product)
+      expect(product.sales.successful.count).to eq(1)
     end
   end
 end
